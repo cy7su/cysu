1ad4637e5ab0ceadf8945cbb6bb96ab5a8dd99b8
cy7su <myaaq@yandex.ru>
2025-09-09 11:27:57 +0000

    update run.py

--- run.py
+++ run.py
@@ -3,14 +3,12 @@ import time
 from multiprocessing import Process
 
 def run_website():
-    """Запуск веб-сайта"""
     from app import create_app
     app = create_app()
     app.run(host='0.0.0.0', port=8001, debug=False)
 
 
 def run_telegram_bot():
-    """Запуск Telegram бота"""
     import logging
     logging.getLogger('telegram_bot').setLevel(logging.CRITICAL)
     logging.getLogger('httpx').setLevel(logging.CRITICAL)
@@ -23,24 +21,19 @@ def run_telegram_bot():
 
 def main():
     """Главная функция"""
-    # Создаем процессы
     website_process = Process(target=run_website)
     bot_process = Process(target=run_telegram_bot)
     
     try:
-        # Запускаем процессы
         website_process.start()
         time.sleep(1)
         bot_process.start()
-        
-        # Ждем завершения процессов
         website_process.join()
         bot_process.join()
         
     except KeyboardInterrupt:
         pass
     finally:
-        # Завершаем процессы
         if website_process.is_alive():
             website_process.terminate()
             website_process.join()
